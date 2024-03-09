29afa064e324eac978fdef58ee8e38cab1186702
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-03-08 22:44:43 -0300

    resolve error in controller

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -8,28 +8,30 @@ class sessionController{
         try {
             const{email,password}= req.body
             
-            const existUser= await User.findAll({where:{
+            const existUser= await User.findOne({where:{
                 email:email
             }})
 
             if(!existUser){
-                return res.send(401).json({message:"user not found", details:false})
+                return res.status(401).json({message:"user not found", details:false})
             }
             
             const secret = process.env.SECRETJWT
 
-            if(await verifyPasswordSecurity(password,existUser[0].password)){
+            if(await verifyPasswordSecurity(password,existUser.password)){
                 const token=  jwt.sign({
-                    username:existUser[0].username,
-                    id:existUser[0].id,
+                    username:existUser.username,
+                    id:existUser.id,
                     exp:Date.now()+60*1000,
                 },secret)
 
+             
+
                 token
-                ? res.status(200).json({message:token,details:true})
-                : res.send(500).json({message:"internal server error",details:false})
+                ? res.status(200).json({message:token,userId: existUser.id,details:true})
+                : res.status(500).json({message:"internal server error",details:false})
             }else{
-                res.send(400).json({message:"password incorrect",details:false})
+                res.status(400).json({message:"password incorrect",details:false})
             }
 
 
