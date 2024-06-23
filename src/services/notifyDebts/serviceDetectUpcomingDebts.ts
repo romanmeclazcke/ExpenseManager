098419fbf95cb462ef93fc86d559b1396b6ca9a3
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-06-22 21:05:10 -0300

    add estrucutre to send email

--- src/services/notifyDebts/serviceDetectUpcomingDebts.ts
+++ src/services/notifyDebts/serviceDetectUpcomingDebts.ts
@@ -1,6 +1,6 @@
 import { Op }  from "sequelize";
 import Debts from "../../models/debtsModel";
-
+import { sendEmail } from "./serviceNotifyUser";
 
 export const getDebtsDueWithinWeek = async () => {
     try {
@@ -12,9 +12,12 @@ export const getDebtsDueWithinWeek = async () => {
                 dueDate: {
                     [Op.gt]: new Date(), // Fecha de vencimiento menor que una semana desde ahora
                     [Op.lt]: oneWeekFromNow, // Fecha de vencimiento una semana mayor a la actual
+                    //hacer inner join con user para acceder directamente al email del user
             }
-        }
-        });
+        }});
+
+        sendEmail() //recibira los datos de la deuda y del user [name, email, toda la info de debts] => se le pasara un arreglo de esta estructura y la log de iteracion sera en el servicio de email
+
         console.log(debts);
     } catch (error) {
         console.error('Error al obtener las deudas:', error);
