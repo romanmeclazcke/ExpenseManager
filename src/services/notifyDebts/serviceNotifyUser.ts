5359bb026f6400700ec115c6415e291929c3df30
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-06-24 10:02:30 -0300

    change view of emails

--- src/services/notifyDebts/serviceNotifyUser.ts
+++ src/services/notifyDebts/serviceNotifyUser.ts
@@ -1,31 +1,65 @@
 import { transporter } from "../../config/db/servisNotifyUser";
 import Debts from "../../models/debtsModel";
 
-export const sendEmail = async (userEmail: string,userName: string,dataDebt: any) => {
+export const sendEmail = async (
+  userEmail: string,
+  userName: string,
+  dataDebt: any
+) => {
   try {
+    const dueDate = new Date(dataDebt.dueDate);
+    const formattedDueDate = `${dueDate.getDate()} ${getMonthName(
+      dueDate.getMonth()
+    )} ${dueDate.getFullYear()}`;
+
     const mailData = {
       from: process.env.EMAILNOTIFY,
       to: userEmail,
       subject: "Notificación de deuda",
       text: `Hola,${userEmail}, posee una deuda proxima a vencer.`,
-      html: `
-            <div style="font-family: Arial, sans-serif; font-size: 14px; color: #333;">
-              <h2 style="color: #337ab7; margin-bottom: 10px;">Notificación de deuda</h2>
-              <p>Posees una deuda próxima a vencer.</p>
-              <table style="width: 100%; border-collapse: collapse; margin-top: 20px;">
-                <tr>
-                  <th style="background-color: #f0f0f0; padding: 10px; border: 1px solid #ddd;">Datos de la deuda</th>
-                </tr>
-                <tr>
-                  <td style="padding: 10px; border: 1px solid #ddd;">
-                    <strong>Nombre de la deuda:</strong> ${dataDebt.name}<br>
-                    <strong>Descripción:</strong> ${dataDebt.description}<br>
-                    <strong>Vencimiento:</strong> ${dataDebt.dueDate}<br>
-                    <strong>Total de la deuda:</strong> ${dataDebt.amount}
-                  </td>
-                </tr>
-              </table>
-            </div>`,
+      html: `<body style="background-color: #f9f9f9; font-family: Arial, sans-serif;">
+  <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" bgcolor="#ffffff" style="border: 1px solid #ddd;">
+    <tbody>
+      <tr>
+        <td align="center">
+          <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" class="col-600">
+            <tbody>
+              <tr>
+                <td align="center" style="background-color: #333; height: 50px; color: #ffffff;">
+                  <a href="#" style="text-decoration: none;">
+                    <p style="font-weight: bold;">Notificación de deuda</p>
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td align="center" style="padding: 20px;">
+                  <p style="font-size: 16px; color: #333;">Posees una deuda próxima a vencer.</p>
+                </td>
+              </tr>
+              <tr>
+                <td align="center" style="padding: 20px; border-top: 1px solid #ddd;">
+                  <table style="width: 100%; border-collapse: collapse;">
+                    <tr>
+                      <th style="background-color: #f0f0f0; padding: 10px; border: 1px solid #ddd;">Datos de la deuda</th>
+                    </tr>
+                    <tr>
+                      <td style="padding: 10px; border: 1px solid #ddd; color: #333;">
+                        <strong>Nombre de la deuda:</strong> ${dataDebt.name}<br>
+                        <strong>Descripción:</strong> ${dataDebt.description}<br>
+                        <strong>Vencimiento:</strong> ${formattedDueDate}<br>
+                        <strong>Total de la deuda:</strong> ${dataDebt.amount}
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>`,
     };
 
     transporter.sendMail(mailData, (error, info) => {
@@ -39,3 +73,21 @@ export const sendEmail = async (userEmail: string,userName: string,dataDebt: any
     console.error("Error en sendEmail:", error);
   }
 };
+
+function getMonthName(month: any) {
+  const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  return meses[month];
+}
