ebe15a539db90e9501ee6dd32f8293f234a1597e
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-10 11:48:35 -0300

    resolve error on generate summary by month

--- src/controllers/summaryController.ts
+++ src/controllers/summaryController.ts
@@ -75,57 +75,83 @@ class SummaryController {
       // Obtener el año actual
       const currentYear = new Date().getFullYear();
 
-      // Consulta para obtener los gastos agrupados por mes del año actual
-      const expenses: any[] = await Expense.findAll({
-        attributes: [
-          [sequelize.fn("DATE_TRUNC", "month", sequelize.col("date")), "month"],
-          [sequelize.fn('SUM', sequelize.col('price')), 'total'],
-        ],
-        where: {
-          idUser: dataUser.id,
-          date: {
-            [Op.between]: [`${currentYear}-01-01`, `${currentYear}-12-31`],
-          },
+      const expenses:any = await Expense.findAll({
+        where:{
+          idUser:dataUser.id,
+          date:{
+            [Op.between]: [`${currentYear}-01-01`, `${currentYear}-12-31`]
+          }
         },
-        group: [sequelize.fn('DATE_FORMAT', sequelize.col('date'), '%Y-%m')],
-        order: [["month", 'ASC']],
-      });
-      console.log("pase")
-      // Consulta para obtener los ingresos agrupados por mes del año actual
-      const incomes: any[] = await Income.findAll({
-        attributes: [
-          [sequelize.fn('DATE_FORMAT', sequelize.col('date'), '%Y-%m'), 'month'],
-          [sequelize.fn('SUM', sequelize.col('price')), 'total'],
+        attributes:[
+          [sequelize.fn('YEAR', sequelize.col('date')), 'year'],
+          [sequelize.fn('MONTH', sequelize.col('date')), 'month'],
+          [sequelize.fn('SUM', sequelize.col('price')), 'total']
         ],
-        where: {
-          idUser: dataUser.id,
-          date: {
-            [Op.between]: [`${currentYear}-01-01`, `${currentYear}-12-31`],
-          },
-        },
-        group: [sequelize.fn('DATE_FORMAT', sequelize.col('date'), '%Y-%m')],
-        order: [["month", 'ASC']],
-      });
+        group: ['year', 'month'],
+        order:[['year', 'ASC'], ['month', 'ASC']]
+      })
 
-      const summary: ExpenseAndIncomeByMonth[] = [];
-      for (let i = 0; i < 12; i++) {
+      const incomes:any = await Income.findAll({
+        where:{
+          idUser:dataUser.id,
+          date:{
+            [Op.between]: [`${currentYear}-01-01`, `${currentYear}-12-31`]
+          }
+        },
+        attributes:[
+          [sequelize.fn('YEAR', sequelize.col('date')), 'year'],
+          [sequelize.fn('MONTH', sequelize.col('date')), 'month'],
+          [sequelize.fn('SUM', sequelize.col('price')), 'total']
+        ],
+        group: ['year', 'month'],
+        order:[['year', 'ASC'], ['month', 'ASC']]
+      })
+      
+      
+      let expenseAndIncome: ExpenseAndIncomeByMonth[]=[];
+      let dolarPrice =  await getPriceDolar();
+      for(let i=1;i<=12;i++){
+        let expensebyMonth= getDataByMonth(expenses,i);
+        let incomebyMonth= getDataByMonth(incomes,i);
+        
+        let expenseInformation = expensebyMonth? expensebyMonth.dataValues.total:0;
+        let incomeInformation = incomebyMonth ? incomebyMonth.dataValues.total : 0;
+
         const month: ExpenseAndIncomeByMonth = {
-          month: getMonthName(i),
-          expensesAmount: expenses[i] || 0, 
-          incomesAmount: incomes[i] || 0,
-        };
-
-        summary.push(month);
+              month: getMonthName(i),
+              totalExpenseARG: expenseInformation,
+              totalIncomeARG: incomeInformation,
+              totalExpenseUSDBLUE: parseFloat(( expenseInformation/ dolarPrice).toFixed(2)),
+              totalIncomeUSDBLUE: parseFloat(( incomeInformation/ dolarPrice).toFixed(2))
+            };
+        expenseAndIncome.push(month);
       }
 
-      summary.length > 0
-        ? res.status(200).json({ message: summary, details: true })
-        : res.status(404).json({ message: "sumary not found", details: false });
-      
+      expenseAndIncome
+      ? res.status(200).json({message:expenseAndIncome,details:true})
+      : res.status(400).json({message:"error",details:false});
+
     } catch (error) {
       res.status(500).json({ message: 'Error fetching summary', details: false });
     }
   }
 }
 
+function getDataByMonth(dataPerMonth:any, indexMonth:number) {
+  if(dataPerMonth.length==12){
+    return dataPerMonth[indexMonth];
+  }else{
+    for(let expense of dataPerMonth){
+      if(expense.dataValues.month>indexMonth){
+        return null; //si el numero del mes por el que estoy pasando es mayor al que busco significa que no esta (retorno null);
+      }
+      if(expense.dataValues.month==indexMonth){ //si encuentro el mes con el indice que busco retorno los datos del mes
+        return expense;
+      }
+    }
+  }
+}
+
+
 export default SummaryController;
+
